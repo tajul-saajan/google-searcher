983e1cc6915928b68bf76a1247d35d4c35ed058e
Tajul Islam <tajul.islam@sigmasolutions.com.bd>
2023-11-21 21:44:51 +0600

    feat(search-stat): added redirect to login if user is not authenticated in is authenticated guard

--- src/guards/is-authenticated.ts
+++ src/guards/is-authenticated.ts
@@ -1,10 +1,19 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { Request } from 'express';
+import { Request, Response } from 'express';
 
 @Injectable()
 export class IsAuthenticatedGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean {
-    const request = context.switchToHttp().getRequest() as Request;
+    const ctx = context.switchToHttp();
+    const request = ctx.getRequest() as Request;
+    const response = ctx.getResponse() as Response;
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    const user = request?.session?.passport?.user;
+
+    if (!user) {
+      response.redirect('/auth/login');
+    }
 
     return request.isAuthenticated();
   }
