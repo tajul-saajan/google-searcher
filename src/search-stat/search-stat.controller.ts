d804877258c8efb1cf5d8eb7bc474359fa1f4c1d
Tajul Islam <tajul.islam@sigmasolutions.com.bd>
2023-11-23 23:09:00 +0600

    feat(pagination): added pagination

--- src/search-stat/search-stat.controller.ts
+++ src/search-stat/search-stat.controller.ts
@@ -8,6 +8,7 @@ import {
 } from '@nestjs/common';
 import { SearchStatService } from './search-stat.service';
 import { IsAuthenticatedGuard } from '../guards/is-authenticated';
+import { Paginate, PaginateQuery } from 'nestjs-paginate';
 
 @Controller('search-stat')
 export class SearchStatController {
@@ -16,8 +17,13 @@ export class SearchStatController {
   @UseGuards(IsAuthenticatedGuard)
   @Render('home')
   @Get()
-  async findMany(@Query() { search }, @Session() session: Record<string, any>) {
-    const results = await this.service.findMany(search);
+  async findMany(
+    @Query() { search },
+    @Session() session: Record<string, any>,
+    @Paginate() query: PaginateQuery,
+  ) {
+    const results = await this.service.findMany(search, query);
+    console.log(results);
     const { data } = session;
     delete session.data;
     return { results, ...data };
--- src/search-stat/search-stat.service.ts
+++ src/search-stat/search-stat.service.ts
@@ -1,18 +1,30 @@
 import { Injectable } from '@nestjs/common';
-import { EntityManager, Like } from 'typeorm';
+import { EntityManager } from 'typeorm';
 import { SearchStat } from '../entities/search-stat.entity';
+import { FilterOperator, paginate, PaginateQuery } from 'nestjs-paginate';
 
 @Injectable()
 export class SearchStatService {
   constructor(private readonly entityManager: EntityManager) {}
 
-  async findMany(search: string) {
+  async findMany(search: string, pgQuery: PaginateQuery) {
     const query = this.entityManager.createQueryBuilder(SearchStat, 'ss');
     if (search) {
       query.where('ss.keyword LIKE :search', { search: `%${search}%` });
     }
 
-    return await query.getMany();
+    return await paginate(pgQuery, query, {
+      filterableColumns: {
+        keyword: [FilterOperator.ILIKE],
+      },
+      searchableColumns: [],
+      select: ['keyword', 'adsCount', 'linksCount', 'totalResultsCount'],
+      sortableColumns: ['id'],
+      defaultSortBy: [['id', 'DESC']],
+      nullSort: 'last',
+    });
+
+    // return await query.getMany();
   }
 
   async getKeywords() {
