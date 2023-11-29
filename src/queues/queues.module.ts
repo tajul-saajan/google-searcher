c4fdc2ae8fd66eb456e4afcaa4c8e6bb3cee3f75
Tajul Islam <tajul.islam@sigmasolutions.com.bd>
2023-11-29 23:46:51 +0600

    feat(search/queue): added attempts, backoff time and exponential delay for failed jobs

--- src/queues/queues.module.ts
+++ src/queues/queues.module.ts
@@ -2,11 +2,22 @@ import { Module } from '@nestjs/common';
 import { BullModule } from '@nestjs/bull';
 import { SearchConsumer } from './search/search.consumer';
 import { SearcherModule } from '../searcher/searcher.module';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    BullModule.registerQueue({
+    BullModule.registerQueueAsync({
       name: 'search',
+      useFactory: async (configService: ConfigService) => ({
+        defaultJobOptions: {
+          attempts: configService.get('BULL_RETRY_ATTEMPTS'),
+          backoff: {
+            type: 'exponential',
+            delay: configService.get('BULL_RETRY_DELAY'),
+          },
+        },
+      }),
+      inject: [ConfigService],
     }),
     SearcherModule,
   ],
