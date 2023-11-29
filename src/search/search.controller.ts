f7d13b167e03566eb575f2a9f854e32da0b1be6f
Tajul Islam <tajul.islam@sigmasolutions.com.bd>
2023-11-29 21:40:54 +0600

    fix(search/file-upload): added validation for maximum allowed keywords in an uploaded csv file

--- src/search/search.controller.ts
+++ src/search/search.controller.ts
@@ -19,6 +19,7 @@ import { SearchService } from './search.service';
 
 @Controller('search')
 export class SearchController {
+  private readonly MAX_ALLOWED_KEYWORDS = 100;
   constructor(
     @Inject(IFileParser) private readonly parser: FileParser,
     private readonly eventEmitter: EventEmitter2,
@@ -38,19 +39,24 @@ export class SearchController {
     @Res() response: Response,
     @Req() request: Request,
   ) {
-    console.log(file);
-    const keywords = await this.parser.parseData<CsvField>(file);
-    this.service.deleteFile();
-    this.eventEmitter.emit('file.uploaded', new FileUploadedEvent(keywords));
-
-    console.log(request.user, 'user');
-
-    // @ts-ignore
-    request.session.data = {
+    const sessionData = {
       success: true,
       message:
         'Your file has been uploaded. Your results will be displayed as they are available',
     };
+    const keywords = await this.parser.parseData<CsvField>(file);
+    this.service.deleteFile();
+    if (keywords.length > this.MAX_ALLOWED_KEYWORDS) {
+      sessionData.success = false;
+      sessionData.message = `Uploaded file should not contain more than ${this.MAX_ALLOWED_KEYWORDS} keywords`;
+    }
+
+    if (sessionData.success)
+      this.eventEmitter.emit('file.uploaded', new FileUploadedEvent(keywords));
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    request.session.data = sessionData;
     response.redirect('/search-stat');
   }
 }
